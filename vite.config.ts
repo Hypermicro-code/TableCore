d216cc439e5e57a190a487b276c052145c282bcc
Tony Mathisen <tony2mat@gmail.com>
2025-10-19 17:31:39 +0200

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Sett base til '/tablecore/' for GitHub Pages prosjekt-side
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.GITHUB_PAGES === 'true' ? '/TableCore/' : '/',
-})
