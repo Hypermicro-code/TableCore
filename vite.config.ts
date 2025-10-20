462ca5f8a9cda03c1b7e9999566197973eb77fb0
Tony Mathisen <tony2mat@gmail.com>
2025-10-20 20:15:16 +0200

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// VIKTIG: Bytt REPO_NAVN til repo-navnet ditt (nøyaktig samme store/små bokstaver)
-export default defineConfig({
-  plugins: [react()],
-  base: '/TableCore/',     // <- må matche GitHub Pages sti
-})
