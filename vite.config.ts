7dcb6eab63ab30d8922e17326e919a70f4330250
Tony Mathisen <tony2mat@gmail.com>
2025-10-20 21:08:51 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import { fileURLToPath } from "node:url"
 
-// For GitHub Pages i et repo (ikke custom domain), bruk base: "/<repo-navn>/"
-// For enkelhet settes base til "./" slik at paths blir relative.
+// For GitHub Pages i et repo (ikke custom domain), bruk base: "./" for relative paths.
 export default defineConfig({
   plugins: [react()],
-  base: "./"
+  base: "./",
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  }
 })
